51313ef21012645d364f2881a46a84e9b76cbf80
Jean-Michel Frouin <jm@frouin.me>
2024-10-07 17:52:45 +0200

    Switch on SDL2, drop Cairo.

--- src/EP.cpp
+++ src/EP.cpp
@@ -1,50 +1,104 @@
-#include <Core/Def.h>
-#include <Core/File.h>
+#include <SDL2/SDL.h>
 #include <iostream>
-#ifdef MACOS
-#include <cairo/cairo-quartz.h>
-#include <cairo/cairo.h>
-#endif
+#include <cmath>
 
-int main()
-{
-    const auto start{std::chrono::steady_clock::now()};
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const int MOVE_STEP = 10;
+const double PI = 3.14159265358979323846;
 
-//#if defined DEBUG
-    std::cout << GREEN << "[DBG] " << RED << FULLNAME << "\n" << STOP;
-    std::cout << GREEN << "[DBG] DEBUG mode\n" << STOP;
-//#endif
+struct Triangle {
+    SDL_Point points[3];
+};
 
-    Core::CFile File;
-    File.Open("EP.txt", Core::CFile::eWrite);
-    File.Write("Hello, World!");
+SDL_Point calculateCenter(const Triangle& triangle) {
+    int x = (triangle.points[0].x + triangle.points[1].x + triangle.points[2].x) / 3;
+    int y = (triangle.points[0].y + triangle.points[1].y + triangle.points[2].y) / 3;
+    return {x, y};
+}
 
-    const auto end{std::chrono::steady_clock::now()};
-    const std::chrono::duration<double> elapsed_seconds{end - start};
-    std::cout << GREEN << "Elapsed time: " << elapsed_seconds.count() << "s\n" << STOP;
+void rotateTriangle(Triangle& triangle, double angle) {
+    SDL_Point center = calculateCenter(triangle);
+    double radians = angle * PI / 180.0;
+    for (int i = 0; i < 3; ++i) {
+        int x = triangle.points[i].x - center.x;
+        int y = triangle.points[i].y - center.y;
+        triangle.points[i].x = center.x + static_cast<int>(x * cos(radians) - y * sin(radians));
+        triangle.points[i].y = center.y + static_cast<int>(x * sin(radians) + y * cos(radians));
+    }
+}
 
-#ifdef MACOS
-    cairo_surface_t* surface = cairo_quartz_surface_create (
-              CAIRO_FORMAT_RGB24
-            , 640
-            , 480
-            );
+void drawTriangle(SDL_Renderer* renderer, const Triangle& triangle) {
+    SDL_RenderDrawLines(renderer, triangle.points, 3);
+    SDL_RenderDrawLine(renderer, triangle.points[2].x, triangle.points[2].y, triangle.points[0].x, triangle.points[0].y);
+}
 
-    cairo_t* cr = cairo_create(surface);
+void moveTriangle(Triangle& triangle, int dx, int dy) {
+    for (int i = 0; i < 3; ++i) {
+        triangle.points[i].x += dx;
+        triangle.points[i].y += dy;
+    }
+}
 
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    cairo_set_source_rgb(cr, 0xFF, 0xFF, 0xFF);
-    cairo_paint(cr);
+    SDL_Window* window = SDL_CreateWindow("eOS poc version", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
-    //draw a line from 0,0 to 100,200
-    cairo_set_source_rgb(cr, 0xFF, 0, 0);
-    cairo_move_to(cr, 0, 0);
-    cairo_line_to(cr, 100, 200);
-    cairo_stroke(cr);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
-    cairo_surface_write_to_png(surface, "cairo.png");
-    cairo_destroy(cr);
-    cairo_surface_destroy(surface);
-#endif
+    Triangle triangle = {{{400, 300}, {450, 350}, {350, 350}}};
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            } else if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_UP:
+                        moveTriangle(triangle, 0, -MOVE_STEP);
+                        break;
+                    case SDLK_DOWN:
+                        moveTriangle(triangle, 0, MOVE_STEP);
+                        break;
+                    case SDLK_LEFT:
+                        rotateTriangle(triangle, -90);
+                        break;
+                    case SDLK_RIGHT:
+                        rotateTriangle(triangle, 90);
+                        break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        drawTriangle(renderer, triangle);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
